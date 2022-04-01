f28171491ea30b323f399a00c420d850509db448
Korbinian Offensperger <korbinian_offensperger@yahoo.de>
2022-04-01 08:46:06 +0200

    Projektdateien hinzufügen.

---
+++ CodeWars_CountSheep_EnumerableRange/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars_CountSheep_EnumerableRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+        }
+
+        // First solution:
+        public static string CountSheep(int n)
+        {
+            if (n == 0)
+            {
+                return String.Empty;
+            }
+            else
+            {
+                string output = String.Empty;
+                for (int ctr = 0; ctr < n; ctr++)
+                    output += $"{ctr + 1} sheep...";
+                return output;
+            }
+        }
+
+        // Refactored version:
+        public static string CountSheepTwo(int n)
+        {
+            return string.Concat(Enumerable.Range(1, n).Select(i => $"{i} sheep..."));
+        }
+    }
+}
